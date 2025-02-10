bd5a1dc2fbe62521bbd5bc8959c73188cb483010
FjoreverFord <foreverforthefjord@gmail.com>
2025-02-09 18:33:28 -0800

    Update config.js

--- config.js
+++ config.js
@@ -50,7 +50,7 @@ const CONFIG = {
     celebration: {
         title: "I'm so happy to be able to spend the day with you!",
         message: "Now come get your gift, a big warm hug and a huge kiss!",
-        emojis: "🙂‍↕️🤗😇💕"  // These will bounce around
+        emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
     // Color scheme for the website
@@ -70,7 +70,8 @@ const CONFIG = {
         floatDistance: "50px",          // How far hearts move sideways (30-70px recommended)
         bounceSpeed: "0.5s",            // Speed of bouncing animations (0.3-0.7s recommended)
         heartExplosionSize: 1.5         // Size of heart explosion effect (1.2-2.0 recommended)
-  },
+    } // <-- This closing bracket was missing
+};
 
 // Don't modify anything below this line unless you know what you're doing
 window.VALENTINE_CONFIG = CONFIG;
