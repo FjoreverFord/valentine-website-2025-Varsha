6c47ca10f4dbdd22bf7d80462f0699a465bd92bf
FjoreverFord <foreverforthefjord@gmail.com>
2025-02-09 18:34:16 -0800

    Update config.js

--- config.js
+++ config.js
@@ -70,7 +70,7 @@ const CONFIG = {
         floatDistance: "50px",          // How far hearts move sideways (30-70px recommended)
         bounceSpeed: "0.5s",            // Speed of bouncing animations (0.3-0.7s recommended)
         heartExplosionSize: 1.5         // Size of heart explosion effect (1.2-2.0 recommended)
-    } // <-- This closing bracket was missing
+    } 
 };
 
 // Don't modify anything below this line unless you know what you're doing
